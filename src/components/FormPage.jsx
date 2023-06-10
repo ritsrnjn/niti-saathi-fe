f5f61da52e81ab3293f2e3a9bb87d294dca14299
Ritesh Ranjan <riteshranjan1620@gmail.com>
2023-06-10 21:59:11 +0530

    added response table

--- src/components/FormPage.jsx
+++ src/components/FormPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form, Spinner, Container, Row, Col, Alert, Card } from 'react-bootstrap';
+import { Button, Form, Spinner, Container, Row, Col, Alert, Card, Table } from 'react-bootstrap';
 import '../styles/FormPage.css';
 import axios from 'axios';
 
@@ -7,9 +7,11 @@ const FormPage = () => {
   const [state, setState] = useState({
     name: '',
     age: '',
+    gender: '',
     salary: '',
     caste: '',
-    remarks: ''
+    state: '',
+    additionalInfo: ''
   });
 
   const [loading, setLoading] = useState(false);
@@ -28,9 +30,10 @@ const FormPage = () => {
     setLoading(true);
     setResponse(null);
 
-    const query = `My name is ${state.name} my age is ${state.age} my salary is ${state.salary} my caste is ${state.caste} ... what policies are applicable for me? Give just the list of policies and theier short summary, if no policies are applicalbe just say "No applicable policies found"`;
+    const query = `My name is ${state.name} my age is ${state.age} my salary is ${state.salary} my caste is ${state.caste} ... Based on the information provided, which policies are applicable to ${state.name}? Please provide a list of up to three policies along with a brief summary of each. Additionally, kindly include some insights on why each policy is helpful and what is the financials benefits"`;
 
     try {
+
       const response = await axios.post('http://127.0.0.1:5000/api', { query });
 
       setResponse(response.data);  // Add this line
@@ -42,7 +45,30 @@ const FormPage = () => {
     } catch (error) {
       console.error(error);
 
-      setResponse(error);
+      // setResponse(error);
+
+      // make a sample response object with the same structure as the response object
+      const errorResponse = {
+        data: 
+          [
+            {
+              policy: 'Policy 1',
+              summary: 'Summary 1',
+              benefits: 'Benefits 1'
+            },
+            {
+              policy: 'Policy 2',
+              summary: 'Summary 2',
+              benefits: 'Benefits 2'
+            }
+          ]
+      };
+
+      setResponse(errorResponse.data);
+      // getting this error
+      // response.map is not a function
+// TypeError: response.map is not a function
+
       setLoading(false);
     }
   };
@@ -113,6 +139,11 @@ return (
                     <Form.Label>Age</Form.Label>
                     <Form.Control type="number" name="age" onChange={handleChange} />
                     </Form.Group>
+
+                    <Form.Group controlId="formGender">
+                    <Form.Label>Gender</Form.Label>
+                    <Form.Control type="number" name="gender" onChange={handleChange} />
+                    </Form.Group>
                     
                     <Form.Group controlId="formSalary">
                     <Form.Label>Salary</Form.Label>
@@ -123,23 +154,60 @@ return (
                     <Form.Label>Caste</Form.Label>
                     <Form.Control type="text" name="caste" onChange={handleChange} />
                     </Form.Group>
+
+                    <Form.Group controlId="formState">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="text" name="state" onChange={handleChange} />
+                    </Form.Group>
+
                 </Col>
                   <Col md={6}>
-                    {/* ... */}
+                    <Form.Group controlId="formRemarks">
+                    <Form.Label>Remarks</Form.Label>
+                    <Form.Control type="text" name="remarks" onChange={handleChange} as="textarea" rows={3} />
+                    </Form.Group>
+
+                  <div className="text-center">
+                  <Button variant="success" type="submit" disabled={loading}>
+                  {loading ? <Spinner animation="border" size="sm" /> : "Submit"}
+                  </Button>
+                  </div>
+
                   </Col>
                 </Row>
-                <Button variant="primary" type="submit" disabled={loading}>
-                  {loading ? <Spinner animation="border" size="sm" /> : 'Submit'}
-                </Button>
+
               </Form>
             </Card.Body>
           </Card>
           {response && 
-            <Alert variant="secondary" className="mt-3 response-box">
-              <Alert.Heading>Policies for you</Alert.Heading>
-              <p>{JSON.stringify(response.data, null, 2)}</p>
-            </Alert>
-          }
+        <Row className="justify-content-md-center mt-3">
+          <Col xs={12} md={8}>
+            <Card className="response-card">
+              <Card.Body>
+                <Card.Title>Policies for you</Card.Title>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Policy</th>
+                      <th>Summary</th>
+                      <th>Benefits</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {response.map((policy, index) => (
+                      <tr key={index}>
+                        <td>{policy.policy}</td>
+                        <td>{policy.summary}</td>
+                        <td>{policy.benefits}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      }
         </Col>
       </Row>
     </Container>
@@ -148,3 +216,26 @@ return (
 
 
 export default FormPage;
+
+
+/*
+Okay, now let's improve the response part
+
+The api will response in this format
+[
+{
+    "policy": "policy1",
+    "summary": "summary1"
+    "benifits": "benifits1"
+},
+{
+    "policy": "policy2",
+    "summary": "summary2"
+    "benifits": "benifits2"
+},
+]
+
+We want to show it as a table on the frontend each element in a row. Make the table look really nice. Use your creativity here
+
+
+*/
